999091c2ccc359c68cf56af755a6bc006a2a1cd7
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-05-22 00:53:34 +0100

    basic variables and constructor

---
+++ contracts/Dex.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// who ever deploys the contract can sell the token, and they can sell any token they want
+// and they way the will specify what they are selling is by sending the address of that token contract to this contract
+// we need to store the token we are selling and the price the user wants to sell the token for
+
+contract Dex {
+    IERC20 public associatedToken;
+    uint256 price;
+    address owner;
+
+    constructor(IERC20 _token, uint256 _price) {
+        associatedToken = _token;
+        price = _price;
+        owner = msg.sender;
+    }
+}
