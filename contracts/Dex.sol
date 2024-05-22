1f304995c5d53e3eb853c5e4067fd350c5959013
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-05-22 21:36:46 +0100

    rewrite sell function

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -2,42 +2,30 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// who ever deploys the contract can sell the token, and they can sell any token they want
-// and they way the will specify what they are selling is by sending the address of that token contract to this contract
-// we need to store the token we are selling and the price the user wants to sell the token for
 
-contract Dex {
+contract Token {
     IERC20 public associatedToken;
     uint256 price;
     address owner;
 
-    constructor(IERC20 _token, uint256 _price) {
-        associatedToken = _token;
-        price = _price;
+    constructor(IERC20 _token, uint256 _amount) {
         owner = msg.sender;
-    }
-    modifier onlyOwner() {
-        require(msg.sender == owner, "only owner can call this contract");
-        _;
+        price = _amount;
+        associatedToken = _token;
     }
 
-    function sell() external onlyOwner {
-        uint256 allowance = associatedToken.allowance(
-            msg.sender,
-            address(this)
-        );
-        require(allowance > 0, "you need to allow at least one token");
+    modifier onlywner() {
+        require(msg.sender == owner);
+        _;
+    }
+    function sell() external onlywner {
+        uint256 allowance = associatedToken.allowance(owner, address(this));
+        require(allowance > 0, "must allow at least one token");
         bool sent = associatedToken.transferFrom(
-            msg.sender,
+            owner,
             address(this),
             allowance
         );
-        require(sent, "transfer failed");
-    }
-
-    function withdrawToken() external onlyOwner {
-        uint balance = associatedToken.balanceOf(address(this));
-        bool sent = associatedToken.transfer(msg.sender, balance);
-        require(sent, "transfer failed");
+        require(sent);
     }
 }
