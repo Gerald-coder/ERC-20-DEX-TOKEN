7e4bfadf334041c86eb649c463b2078d297233f3
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-05-22 21:51:34 +0100

    revert to initial contract

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -2,35 +2,42 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// who ever deploys the contract can sell the token, and they can sell any token they want
+// and they way the will specify what they are selling is by sending the address of that token contract to this contract
+// we need to store the token we are selling and the price the user wants to sell the token for
 
-contract Token {
+contract Dex {
     IERC20 public associatedToken;
     uint256 price;
     address owner;
 
-    constructor(IERC20 _token, uint256 _amount) {
-        owner = msg.sender;
-        price = _amount;
+    constructor(IERC20 _token, uint256 _price) {
         associatedToken = _token;
+        price = _price;
+        owner = msg.sender;
     }
-
-    modifier onlywner() {
-        require(msg.sender == owner);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can call this this");
         _;
     }
-    function sell() external onlywner {
-        uint256 allowance = associatedToken.allowance(owner, address(this));
-        require(allowance > 0, "must allow at least one token");
+
+    function sell() external onlyOwner {
+        uint256 allowance = associatedToken.allowance(
+            msg.sender,
+            address(this)
+        );
+        require(allowance > 0, "you need to allow at least one token");
         bool sent = associatedToken.transferFrom(
-            owner,
+            msg.sender,
             address(this),
             allowance
         );
-        require(sent);
+        require(sent, "transfer failed");
     }
 
-    function withdrawToken() external onlywner {
+    function withdrawToken() external onlyOwner {
         uint balance = associatedToken.balanceOf(address(this));
-        associatedToken.transfer(owner, balance);
+        bool sent = associatedToken.transfer(msg.sender, balance);
+        require(sent, "transfer failed");
     }
 }
