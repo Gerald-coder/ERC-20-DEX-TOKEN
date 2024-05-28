acc3c4dae74960d1d4a18beba4aa08405fcc2a2c
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-05-28 23:15:40 +0100

    update code for taste cases

--- test/Dex.js
+++ test/Dex.js
@@ -15,14 +15,31 @@ describe("Dex", () => {
     const token = await Token.deploy(totalSupply);
 
     const Dex = await ethers.getContractFactory("Dex");
-    const dex = await Dex.deploy(token.address, price);
+    const dex = await Dex.deploy(token, price);
     return { dex, token, owner, addr1, addr2 };
   }
+
   describe("Selling", () => {
     it("should fail to sell", async () => {
-      const { dex, owner } = await loadFixture(deployDex);
-      expect(await dex.connect(owner).sell()).to.be.reverted;
+      const { dex, owner, token, addr1 } = await loadFixture(deployDex);
+      await expect(dex.connect(owner).sell()).to.be.reverted;
       //STOP
     });
+    // it("should allow Dex to sell token", async () => {
+    //   const { dex, owner, token, addr1 } = await loadFixture(deployDex);
+    //   await token.connect(owner).approve(dex.address, 100);
+    // });
+    // it("should not allow non-owners to sell", async () => {
+    //   const { dex, owner, token, addr1 } = await loadFixture(deployDex);
+    //   await expect(dex.connect(addr1).sell()).to.be.reverted;
+    // });
+    // it("should send token from owner to contract", async () => {
+    //   const { dex, owner, token, addr1 } = await loadFixture(deployDex);
+    //   await expect(dex.sell()).to.changeTokenBalances(
+    //     token,
+    //     [owner.address, dex.address],
+    //     [-100, 100]
+    //   );
+    // });
   });
 });
